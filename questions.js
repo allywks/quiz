9d9f9927c5870a394d8eeefb2d54bd008adc2563
allywks <imallyss2@gmail.com>
2019-05-21 13:45:33 -0300

    adicionado o projeto

---
+++ questions.js
@@ -0,0 +1,23 @@
+var questions = [
+
+    ["(Unicamp 2019) No triângulo exibido na figura a seguir, é a bissetriz do ângulo interno em AD=DB. O Ângulo em A é igual a:",
+        "60°", "70°", "80°", "90°","100°",
+        "img/ex1.jpg", "", "", "", "","",
+        "3"
+        
+    ],
+
+    ["(Enem 2018) Os guindastes são fundamentais em canteiros de obras, no manejo de materiais pesados como vigas de aço. A figura ilustra uma sequência de estágios em que um guindaste iça uma viga de aço que se encontra inicial mente no solo. Na figura, o ponto 'O' representa a projeção ortogonal do cabo de aço sobre o plano do chão e este se mantém na vertical durante todo o movimento de içamento da viga, que se inicia no tempo 't = 0' (estágio 1) e finaliza no tempo 'Tf' (estágio 3). Uma das extremidades da viga é içada verticalmente a partir do ponto 'O' enquanto que a outra extremidade desliza sobre o solo em direção ao ponto 'O'. Considere que o cabo de aço utilizado pelo guindaste para içar a viga fique sempre na posição vertical. Na figura, o ponto 'O' representa o ponto médio do segmento que representa a viga. O gráfico que descreve a distância do ponto 'M' ao ponto 'O' em função do tempo, entre 't = 0' e 'Tf' é ",
+        "", "", "", "","",
+        "img/ex2.jpg", "img/Aex2.jpg", "img/Bex2.jpg", "img/Cex2.jpg", "img/Dex2.jpg","img/Eex2.jpg",
+        "1"
+
+    ],
+    ["(Espm 2016)  Num mapa, uma estrada retilínea passa sucessivamente pelas cidades 'A', 'B' e 'C' e uma cidade 'D', distante 120km de 'A', está localizada de tal forma que o ângulo 'DAB' mede 36°. Um viajante fez o trajeto 'AB', 'BD' e 'DC', percorrendo em cada trecho a mesma distância. Se ele tivesse ido diretamente de 'A' até 'C' teria percorrido uma distância de: ",
+    "", "", "", "","",
+    "img/ex2.jpg", "img/Aex3.jpg", "img/Bex3.jpg", "img/Cex3.jpg", "img/Dex3.jpg","img/Eex3.jpg",
+    "3"
+
+    ],
+
+]
